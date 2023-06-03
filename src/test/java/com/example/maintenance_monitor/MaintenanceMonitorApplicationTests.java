88615f05da4e4cab93c8495423fe9f62406f492d
YOUR NAME <YOUR_EMAIL>
2023-06-03 20:11:31 +0200

    Add test cases for setMessage() and reset() methods

--- src/test/java/com/example/maintenance_monitor/MaintenanceMonitorApplicationTests.java
+++ src/test/java/com/example/maintenance_monitor/MaintenanceMonitorApplicationTests.java
@@ -2,12 +2,25 @@ package com.example.maintenance_monitor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MaintenanceMonitorApplicationTests {
-
+	private MaintenanceMonitorMessageEdits maintenanceMonitor;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void testSetMessage() {
+		String message = "New message";
+		maintenanceMonitor.setMessage(message);
+		assertEquals(message, maintenanceMonitor.getMessage());
+	}
+	@Test
+	public void testReset() {
+		maintenanceMonitor.setMessage("Some message");
+		maintenanceMonitor.reset();
+		assertEquals("Everything operates as expected", maintenanceMonitor.getMessage());
 	}
 
 }
